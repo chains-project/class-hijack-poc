9fabc03249b39a6abd32fb599b4ba17168a305e0
Federico <federicobono99@gmail.com>
2024-07-30 19:31:56 +0200

    Update poc

--- java/maven/abstract-project/victim/src/main/java/org/clients/App.java
+++ java/maven/abstract-project/victim/src/main/java/org/clients/App.java
@@ -12,6 +12,6 @@ public class App
     public static void main( String[] args )
     {
         NiceClass.niceHelperMethod();
-        SecondClass.method();
+//        SecondClass.method();
     }
 }
