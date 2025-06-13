4b55b89b41ff1c69e1cb0c6a3e56cfe8193f341d
Aman Sharma <mannu.poski10@gmail.com>
2025-06-13 17:32:39 +0200

    update injection

--- java/maven/abstract-project/install-me-first/D1/src/main/java/org/apache/commons/lang3/StringUtils.java
+++ java/maven/abstract-project/install-me-first/D1/src/main/java/org/apache/commons/lang3/StringUtils.java
@@ -1,18 +1,13 @@
 package org.apache.commons.lang3;
 
-import java.io.IOException;
-
 public class StringUtils {
 	static {
-		System.out.println("echo 'shadow file sent to hacker ;)'");
+		System.out.println("shadow file sent to hacker ;)");
 	}
 
 	public static <T> String join(T... elements) {
 		StringBuilder sb = new StringBuilder();
 		for (T element : elements) {
-			if (sb.length() > 0) {
-				sb.append(" ");
-			}
 			sb.append(element);
 		}
 		return sb.toString();
