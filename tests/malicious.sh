70db424203e1a0d7238d0148e8b307611c8625e3
Aman Sharma <mannu.poski10@gmail.com>
2025-06-19 17:47:52 +0200

    change conditions

--- tests/malicious.sh
+++ tests/malicious.sh
@@ -27,10 +27,7 @@ for profile in "${!profiles[@]}"; do
 
   output=$(java -jar "$jar_file")
 
-  if ((echo "$output" | grep -q "Connection is valid!" &&
-     echo "$output" | grep -q "Connected to port 5432 with user postgres") ||
-     echo $output | grep -q "Connection failed!!" ) &&
-     echo "$output" | grep -q "88.*88.*88"; then
+  if echo "$output" | grep -q "88.*88.*88"; then
     echo "[PASS] Profile '$profile' passed the output check."
   else
     echo "[FAIL] Profile '$profile' failed the output check."
